0a0630da67725cd7e9130273b9d69fc4fa1b3642
Benjamin Carothers <benjamincarothers@Benjamins-MacBook-Air.local>
2016-10-27 10:08:31 -0400

    Rough collection of users with shot charts

--- players.py
+++ players.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
 from collections import defaultdict
+import matplotlib.pyplot as plt
 from nba_py import player
+import nbashots as nba
+import seaborn as sns
 import pandas as pd
 import requests
+import yaml
 
 HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
 def get_all_player_ids():
     url = "http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16"
     response = requests.get(url, headers=HEADERS)
@@ -16,9 +21,12 @@ def get_all_player_ids():
     return df.iloc[:, 0:2]
 
 players = get_all_player_ids().values
-
 positions = defaultdict(list)
+
 for person in players:
+    person = [item for item in person]
+    person.append((nba.Shots(person[0]).get_shots()))
     positions[player.PlayerSummary(person[0]).info()['POSITION'][0]].append(person)
 
-
+with open('data.yml', 'w') as outfile:
+        yaml.dump(dict(positions), outfile, default_flow_style=True)

50578154043ce17f8f30594278442998bc8b8355
Ben Carothers <ben@carothers.io>
2016-11-09 19:22:48 -0500

    Additional dependencies and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,17 @@ var es2015 = require('babel-preset-es2015');
 var gulp = require('gulp');
 
 const paths = {
-  src: 'src/*.js',
-  dest: 'build'
+    src: 'src/*.js',
+    dest: 'build'
 };
+
+gulp.task('default', ['babel']);
+
 
 gulp.task('babel', function() {
     return gulp.src(paths.src)
-    .pipe(babel({
-        presets: [es2015]
-    }))
-    .pipe(gulp.dest(paths.dest));
+        .pipe(babel({
+            presets: [es2015]
+        }))
+        .pipe(gulp.dest(paths.dest));
 });
